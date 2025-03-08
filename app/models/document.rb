76db384267391cdd247795677140ab4c2cf90a49
kenta-crv <okuyamakenta@gmail.com>
2025-03-08 13:03:11 +0900

    change the company

--- app/models/document.rb
+++
@@ -1,6 +0,0 @@
-class Document < ApplicationRecord
-  validates :company, {presence: true}
-  validates :name, {presence: true}
-  validates :tel, {presence: true}
-  validates :email, {presence: true}
-end
--- app/models/estimate.rb
+++
@@ -1,9 +0,0 @@
-class Estimate < ApplicationRecord
-  validates :company, {presence: true} #会社名
-  validates :name, {presence: true} #担当者名
-  validates :tel, {presence: true} #電話番号
-  validates :mail, {presence: true} #メールアドレス
-  validates :address, {presence: true} #住所
-  validates :url, {presence: true} #URL
-  validates :business, {presence: true} #アポ希望ビジネス
-end
